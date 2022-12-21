41c5e4a35fc50c65f5826803334e2479dea4d1e7
John Reiser <jreiser@BitWagon.com>
2022-12-21 08:22:29 -0800

    MSVC _ReturnAddress() for GCC _builtin_return_address(0)
    	modified:   util/membuffer.cpp

--- src/util/membuffer.cpp
+++ src/util/membuffer.cpp
@@ -32,6 +32,9 @@
 void *membuffer_get_void_ptr(MemBuffer &mb) { return mb.getVoidPtr(); }
 unsigned membuffer_get_size(MemBuffer &mb) { return mb.getSize(); }
 
+#if _MSC_VER && !__INTEL_COMPILER //{
+#define _builtin_return_address(level) _ReturnAddress()
+#endif //}
 /*************************************************************************
 // bool use_simple_mcheck()
 **************************************************************************/
